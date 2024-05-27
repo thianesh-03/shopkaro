5250615172f3226f2046bf5a32953ef722423a0d
thianesh-03 <thianesh2003@gmail.com>
2024-05-27 18:14:54 +0530

    shopkaro commits

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterOutlet],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
 }
