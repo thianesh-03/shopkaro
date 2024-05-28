7cda460eb9fd2d7785162c36aa28c2e4f24b40ed
thianesh-03 <thianesh2003@gmail.com>
2024-05-28 14:31:49 +0530

    Feat: search box added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink,RouterOutlet],
+  imports: [RouterLink,RouterOutlet,FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  constructor(private router: Router) {}
+  
+    onSearch() {
+      this.router.navigate(['login']);
+    }
 }
