9061dabe4d68c94a1e46987d30a24df06daa4fd3
thianesh-03 <thianesh2003@gmail.com>
2024-07-03 22:26:20 +0530

    filter option added

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -13,13 +13,46 @@ import { RouterLink } from '@angular/router';
 })
 export class ProductsComponent implements OnInit {
   products: any[] = [];
+  filteredProducts: any[] = [];
+  filter: string = 'all';
 
-  constructor(private productsService: ProductsService) {}
+  constructor(private productService: ProductsService) {}
 
   ngOnInit(): void {
-    this.productsService.getProducts().subscribe((data) => {
-      console.log(data); // Log product data to inspect structure
+    this.productService.getProducts().subscribe((data: any[]) => {
       this.products = data;
+      this.filteredProducts = data;
     });
   }
+
+  onFilterChange(event: any): void {
+    this.filter = event.target.value;
+    this.applyFilter();
+  }
+
+  applyFilter(): void {
+    switch (this.filter) {
+      case 'all':
+        this.filteredProducts = this.products;
+        break;
+      case 'price':
+        this.filteredProducts = this.products.sort((a, b) => a.price - b.price);
+        break;
+      case 'category':
+        this.filteredProducts = this.products.sort((a, b) => a.category.localeCompare(b.category));
+        break;
+      case 'rating':
+        this.filteredProducts = this.products.sort((a, b) => b.rating.rate - a.rating.rate);
+        break;
+      case 'brand':
+        this.filteredProducts = this.products.sort((a, b) => a.brand.localeCompare(b.brand));
+        break;
+      case 'stock':
+        this.filteredProducts = this.products.sort((a, b) => b.stock - a.stock);
+        break;
+      default:
+        this.filteredProducts = this.products;
+        break; 
+    }
+  }
 }
